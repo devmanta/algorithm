2eb8db64a99bf6518e7a41913b201ebd89b8e692
KimHeeJin <dlwlrgkwk82@gmail.com>
2022-10-23 16:33:50 +0900

    [이진트리] 전위순회, 중위순회, 후위순회

---
+++ src/day221023/BinaryTree.java
@@ -0,0 +1,46 @@
+package day221023;
+
+class Node {
+    int data;
+    Node lt;
+    Node rt;
+
+    public Node(int data) {
+        this.data = data;
+        this.lt = null;
+        this.rt = null;
+    }
+}
+
+public class BinaryTree {
+
+    Node root;
+
+    public void DFS(Node root) {
+        if(root == null) {
+            return;
+        }
+
+        System.out.println(root.data + " ");  //전위 순회
+        DFS(root.lt);
+        System.out.println(root.data + " ");  //중위 순회
+        DFS(root.rt);
+        System.out.println(root.data + " ");  //후위 순회
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(2);
+
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+
+        tree.DFS(tree.root);
+    }
+
+}
